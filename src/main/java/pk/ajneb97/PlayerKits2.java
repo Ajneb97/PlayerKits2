f50e282982714dc46c0e91c10c2ac414321cce5e
Ajneb97 <ajneb97@gmail.com>
2025-03-30 15:26:13 -0300

    1.17.2

--- src/main/java/pk/ajneb97/PlayerKits2.java
+++ src/main/java/pk/ajneb97/PlayerKits2.java
@@ -135,6 +135,9 @@ public class PlayerKits2 extends JavaPlugin {
             case "1.21.4":
                 serverVersion = ServerVersion.v1_21_R3;
                 break;
+            case "1.21.5":
+                serverVersion = ServerVersion.v1_21_R4;
+                break;
             default:
                 serverVersion = ServerVersion.valueOf(packageName.replace("org.bukkit.craftbukkit.", ""));
         }
